fd530223ba1c98bd6f21e2b2f91b2e86388aed26
rockclimber112358 <rockclimber112358@gmail.com>
2015-12-24 10:34:34 -0700

    Exporting na2zero function

--- R/na2zero.R
+++ R/na2zero.R
@@ -7,6 +7,8 @@
 ##' @return The value of x if x has a value and 0 if x is na or has length 0. 
 ##'   If x has length >1, an error is thrown.
 ##' 
+##' @export
+##' 
 
 na2zero = function(x){
     if(length(x) == 0) # No data for variable, return 0
