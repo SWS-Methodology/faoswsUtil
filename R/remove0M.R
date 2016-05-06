e904741c6a0536cb1f9eca2c215e1e9f6708d9ae
mk <mkao006@gmail.com>
2016-05-06 14:36:03 +0200

    moved helper function from faoswsProduction package

--- R/remove0M.R
+++ R/remove0M.R
@@ -1,13 +1,25 @@
-##' Function to remove zero values which are missing.
+##' This function replaces value which are zero and has flag
+##' representing missing values with NA.
+##'
+##' TODO (Michael): This function is similar to the function in the
+##'                 \pkg{faoswsUtil} package, but enables multiple
+##'                 value and flag pairs and also returning the data
+##'                 object. This function should replace the original
+##'                 one in the \pkg{faoswsUtil} package
 ##'
 ##' @param data The data.table object containing the values.
-##' @param value The value of the observation.
-##' @param flag The flag of the observation.
-##' @param naFlag The value of the flag which denotes missing value.
+##' @param valueVars The value of the observation.
+##' @param flagVars The flag of the observation.
+##' @param missingFlag The value of the flag which denotes missing value.
 ##'
+##' @return The data object with all the (0, M) replaced with (NA, M)
 ##' @export
+##'
 
-remove0M = function(data, value, flag, naFlag = "M"){
-    missingIndex = which(data[[flag]] == naFlag)
-    invisible(data[missingIndex, `:=`(c(value), list(NA))])
+remove0M = function(data, valueVars, flagVars, missingFlag = "M"){
+    dataCopy = copy(data)
+    mapply(FUN = function(value, flag){
+        dataCopy[which(dataCopy[[flag]] == missingFlag), `:=`(c(value), NA_real_)]
+    }, value = valueVars, flag = flagVars)
+    dataCopy
 }
