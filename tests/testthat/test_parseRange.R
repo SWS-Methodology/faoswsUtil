c7b008890230b18be8bd5b8bb03ac1ecf0ea43cf
Sebastian Campbell <sebastian.campbell@fao.org>
2017-11-16 14:49:35 +0100

    New function parseRange that allows splitting of a text range into character numbers. Useful for years. Closes #29.

---
+++ tests/testthat/test_parseRange.R
@@ -0,0 +1,26 @@
+context("parseRange")
+
+test_that("parseRange works on edge cases", {
+  expect_identical(parseRange(""), character())
+  expect_identical(parseRange(character()), character())
+})
+
+test_that("parseRange works on single cases", {
+    expect_identical(parseRange("1"), "1")
+    expect_identical(parseRange("1, 2"), c("1", "2"))
+    expect_identical(parseRange("1,2"), c("1", "2"))
+    expect_identical(parseRange("1-3"), c("1", "2", "3"))
+})
+
+test_that("parseRange works on mixed ranges"){
+    expect_identical(parseRange("1-3, 3"), c("1", "2", "3", "3"))
+    expect_identical(parseRange("1-3,5"), c("1", "2", "3", "5"))
+    expect_identical(parseRange("1-3,5, 0"), c("1", "2", "3", "5", "0"))
+}
+
+test_that("parseRange works with different separators", {
+    expect_identical(parseRange("1; 2", sep = "; *"), c("1", "2"))
+    expect_identical(parseRange("2*3", rsep = "\\* *"), c("2", "3"))
+    expect_identical(parseRange("2*3;0", sep = "; *", rsep = "\\* *"), 
+                     c("2", "3", "0"))
+})
